afcee8b1966d9a2b3e3e37baed194a4a8af66f0a
Nikhil Kumar <nikhil80845@gmail.com>
2025-09-24 22:48:09 +0530

    minor change

--- internal/auth/auth_test.go
+++ internal/auth/auth_test.go
@@ -8,7 +8,7 @@ import (
 func TestSplit(t *testing.T) {
 	headers := make(http.Header)
 	apiKey := "sk-12i9319"
-	headers.Add("Authorization", "Api "+apiKey)
+	headers.Add("Authorization", "ApiKey "+apiKey)
 	resApiKey, err := GetAPIKey(headers)
 	if err != nil {
 		t.Fatalf("Error encountered %v", err)
