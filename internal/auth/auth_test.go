e861b2786b87e9a7ce1ac8384189888026fcba75
Nikhil Kumar <nikhil80845@gmail.com>
2025-09-24 22:46:46 +0530

    Adds test for auth

---
+++ internal/auth/auth_test.go
@@ -0,0 +1,19 @@
+package auth
+
+import (
+	"net/http"
+	"testing"
+)
+
+func TestSplit(t *testing.T) {
+	headers := make(http.Header)
+	apiKey := "sk-12i9319"
+	headers.Add("Authorization", "ApiKey "+apiKey)
+	resApiKey, err := GetAPIKey(headers)
+	if err != nil {
+		t.Fatalf("Error encountered %v", err)
+	} else if resApiKey != apiKey {
+		t.Fatalf("Api key mismatch")
+	}
+
+}
